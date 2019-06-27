484ecb8059419e89483871add00e4edc842abf7a
alex fox <to.alexfox@gmail.com>
2019-06-27 16:45:46 -0500

    test dispatch actions - working

--- src/index.js
+++ src/index.js
@@ -7,7 +7,32 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import todoApp from './redux/reducers'
 
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './redux/actions'
+
 const store = createStore(todoApp)
+
+// Log the initial state
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
