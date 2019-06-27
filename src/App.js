5f4b480ac3774c0b550e693dbf7b90d2d71127e7
alex fox <to.alexfox@gmail.com>
2019-06-27 17:34:37 -0500

    add container components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
 function App() {
   return (
     <div className="App">
       hi
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
     </div>
   );
 }
