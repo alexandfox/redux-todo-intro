0bc8a29d64fbc72ea72f8f29fe40d3a6d29ffc01
alex fox <to.alexfox@gmail.com>
2019-06-28 14:14:11 -0500

    pass store through Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from "react-redux"
 import { createStore } from 'redux'
 import todoApp from './redux/reducers'
+import App from "./App"
 
 import {
   addTodo,
@@ -16,23 +18,31 @@ import {
 
 const store = createStore(todoApp)
 
-// Log the initial state
-console.log(store.getState())
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-// Dispatch some actions
-store.dispatch(addTodo('Learn about actions'))
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
-store.dispatch(toggleTodo(0))
-store.dispatch(toggleTodo(1))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-// Stop listening to state updates
-unsubscribe()
-
-ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
+
+// // Log the initial state
+// console.log(store.getState())
+
+// // Every time the state changes, log it
+// // Note that subscribe() returns a function for unregistering the listener
+// const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// // Dispatch some actions
+// store.dispatch(addTodo('Learn about actions'))
+// store.dispatch(addTodo('Learn about reducers'))
+// store.dispatch(addTodo('Learn about store'))
+// store.dispatch(toggleTodo(0))
+// store.dispatch(toggleTodo(1))
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// // Stop listening to state updates
+// unsubscribe()
+
+
